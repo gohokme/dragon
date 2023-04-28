16f8c156e6625195619b218565f5bcb97b560176
gohokme <111654745+gohokme@users.noreply.github.com>
2023-04-28 16:43:28 +0900

    Update values.py

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -117,6 +117,7 @@ CAR_INFO: Dict[str, Union[GMCarInfo, List[GMCarInfo]]] = {
   CAR.EQUINOX: GMCarInfo("Chevrolet Equinox 2019-22"),
   CAR.TRAILBLAZER: [
     GMCarInfo("Chevrolet Trailblazer 2022", "RS Trim with Adaptive Cruise Control Package"),
+  ],
   CAR.TRAX: [
     GMCarInfo("Chevrolet Trax 2023", "RS Trim with Adaptive Cruise Control Package"),
   ],
